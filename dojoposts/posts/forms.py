18094543653d7e05de9ad167e9babedd0b2e320a
rairom <rairom@sonity.net>
2021-11-26 14:02:16 +0200

    max length = 255

--- dojoposts/posts/forms.py
+++ dojoposts/posts/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class PostForm(FlaskForm):
-    post = TextAreaField('Post', validators=[DataRequired()])
+    post = TextAreaField('Post', validators=[Length(max=255)])
     submit = SubmitField('Post')
