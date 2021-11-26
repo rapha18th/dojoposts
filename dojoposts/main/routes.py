4fdc1e6f4165b380ac8d0703e3966fe3aaef4394
rairom <rairom@sonity.net>
2021-11-26 13:31:15 +0200

    post on landing page

--- dojoposts/main/routes.py
+++ dojoposts/main/routes.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from dojoposts.models import Post
 
 main = Blueprint('main', __name__)
 
 # main route
 @main.route('/')
 def index():
-    return render_template('index.html', title='Posts:.')
+    posts = Post.query.order_by(Post.date_posted.desc())
+    posts.paginate()
+    return render_template('index.html', title='Posts:.', posts=posts)
